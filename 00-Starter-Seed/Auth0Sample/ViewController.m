a8cad3e1444bb3d7e7f03ac1fa0cecf1421ebf2e
Sebastian Cancinos <sebacancinos@gmail.com>
2016-06-23 10:47:02 -0300

    Empty template project

---
+++ 00-Starter-Seed/Auth0Sample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Auth0Sample
+//
+//  Created by Sebastian Cancinos on 6/22/16.
+//  Copyright © 2016 Auth0. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
