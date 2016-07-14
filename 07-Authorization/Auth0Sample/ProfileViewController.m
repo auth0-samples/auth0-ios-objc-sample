5d0feaec8368a4c3e760df56bb5f3cb71087ed99
Sebastian Cancinos <sebacancinos@gmail.com>
2016-07-14 13:12:12 -0300

    Fixes on authorisation check, alert and Storyboard

--- 07-Authorization/Auth0Sample/ProfileViewController.m
+++ 07-Authorization/Auth0Sample/ProfileViewController.m
@@ -52,8 +52,14 @@
 - (BOOL)shouldPerformSegueWithIdentifier:(NSString *)identifier sender:(id)sender{
     
     if([identifier isEqualToString:@"AdminSegue"]){
-        if(![self.userProfile.appMetadata[@"roles"] isEqualToString:@"admin"]){
+        if(![self.userProfile.appMetadata[@"roles"] containsObject:@"admin"]){
             UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Access denied" message:@"You do not have privileges to access the admin panel" preferredStyle:UIAlertControllerStyleAlert];
+            UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK"
+                                                                    style:UIAlertActionStyleDefault
+                                                                  handler:^(UIAlertAction * action) {}];
+            
+            [alert addAction:defaultAction];
+
             [self presentViewController:alert animated:YES completion:nil];
             
             return NO;
