9d62520ef7f115ac7283aaef50740bb1738f0f80
Sebastian Cancinos <sebacancinos@gmail.com>
2016-08-12 12:50:56 -0300

    Added customised design to Custom Login sample code

---
+++ 02-Custom-Login/Auth0Sample/UIColor+extraColors.h
@@ -0,0 +1,16 @@
+//
+//  UIColor+extraColors.h
+//  Auth0Sample
+//
+//  Created by Sebastian Cancinos on 8/12/16.
+//  Copyright © 2016 Auth0. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (extraColors)
+
++ (UIColor*) lightVioletColor;
++ (UIColor*) darkVioletColor;
+
+@end
